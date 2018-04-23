36fdf83d443ab8e7413f8a5fae0c7b2551c1b791
EclatOne <kiromesi@yandex.by>
2018-04-23 18:39:44 +0100

    komentarii

--- Proekt1_0/Proekt1_0/Program.cs
+++ Proekt1_0/Proekt1_0/Program.cs
@@ -6,39 +6,61 @@ using System.Threading.Tasks;
 
 namespace Proekt1_0
 {
+	/// <summary>
+	/// класс Страна где описаны наши переменные 
+	/// </summary>
 	class Strana
-	{
+	{ 
 		public string Nazvanie { get; set; }
-		public string FormaPravlenie { get; set; }
+		public string FormaPravlenie { get; set; }  
 		public float Plojad { get; set; }
-		public Strana (string nazvanie, string formaPravlenie, float plojad)
+		/// <summary>
+		/// Конструктор с параметрами
+		/// </summary>
+		/// <param name="nazvanie"></param>
+		/// <param name="formaPravlenie"></param>
+		/// <param name="plojad"></param>
+		public Strana (string nazvanie, string formaPravlenie, float plojad) 
 		{
 			Nazvanie = nazvanie;
 			FormaPravlenie = formaPravlenie;
 			Plojad = plojad;
 
 		}
-		public Strana()
+		/// <summary>
+		/// Конструктор без параметра
+		/// </summary>
+		public Strana() 
 		{
 			Nazvanie = "";
 			FormaPravlenie = "";
 			Plojad = 0;
 		}
+		/// <summary>
+		/// Метод вызова конструктора по умолчанию
+		/// </summary>
 		public void input()
 		{
 			Console.Write("Название:");
 			Console.Write("Форма правления:");
 			Console.Write("Площадь:");
 		}
-		public override string ToString()
+		/// <summary>
+		/// Метод вызова конструктора с параметрами
+		/// </summary>
+		/// <returns></returns>
+		public override string ToString() 
 		{
 			return String.Format("Название: {0} Форма правления: {1} Площадь: {2}", Nazvanie, FormaPravlenie, Plojad);
 		}
 
 	}
+	/// <summary>
+	/// главный класс проекта
+	/// </summary>
 	class Program
 	{
-		static void Main(string[] args)
+		static void Main(string[] args) 
 		{
 			Strana strana = new Strana();
 			Console.WriteLine("Первая страна " + strana);
